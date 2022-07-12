48d5e58f9c6d44efb19a068599f11b42eb209f22
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-12 21:11:57 +0600

    client site data send in the server site

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app=express();
 const http = require('http');
 const expressServer =http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + "/index.html");
 })
+
+
+io.on('connection',(socket)=>{
+    console.log('New user connect');
+    socket.on('chat',function(msg){
+        console.log(msg);
+    })
+})
+
 
 expressServer.listen(5000,function(){
     console.log('server run @5000')
