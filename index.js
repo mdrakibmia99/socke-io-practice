6f699d3a9449bd21c287a7d327d1d0c41dc64f31
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-12 20:30:34 +0600

    index.html file connect with index.js file and print a message

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app=express();
 const http = require('http');
 const expressServer =http.createServer(app);
 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
+
 expressServer.listen(5000,function(){
     console.log('server run @5000')
 })
